b355328fdfd65e367293332e68dc64ca4c4c985a
Volodymyr Gerun <kas1qd0s1q@gmail.com>
2022-08-18 23:26:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+)
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+)
+$('svg').click(() => {
+  console.log(1)
+  $('body').toggleClass('body-js').css('transition', '0.5s ease-in-out')
+  $('h2, p, .bold').toggleClass('text-js')
+  $('svg').toggleClass('night-mode-js').css('transition', '0.1s ease-in-out')
+})
